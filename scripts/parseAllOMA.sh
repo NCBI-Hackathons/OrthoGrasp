1c7a7b7a795f3a4d498300aa8e4a2aa01e422142
Phillip Rak <rak.phillip@gmail.com>
2018-07-12 17:06:16 -0500

    --Created workdir variable
      This might be a better solution when defining the directory to store
      our data, as we only need to upate the path in a single location

--- scripts/parseAllOMA.sh
+++ scripts/parseAllOMA.sh
@@ -7,31 +7,33 @@ touch failedOMAparses.txt
 # gunzip oma-ensembl.txt.gz
 # cd ..
 
+workdir = /OrthoGrasp/data/
+
 # This effectively sets which species OMA ids can be converted from.
 for species in "CAEEL" "DANRE" "DROME" "HUMAN" "MOUSE" "PANTR" "RATNO" "TAKRU"
 do
-    grep ^$species data/oma-ensembl.txt > data/$species-ensembl.sub.txt
+    grep ^$species $workdir+=oma-ensembl.txt > $workdir+=$species-ensembl.sub.txt
 done
 
 # For each of those species files defined above, find the species names and
 # run the parsing perl script for each pair of species.
-for speciesFile1 in data/*-ensembl.sub.txt
+for speciesFile1 in $workdir+=*-ensembl.sub.txt
 do
     species1=$(basename $speciesFile1)
     species1=${species1//-ensembl.sub.txt/}
     echo $species1
-    for speciesFile2 in data/*-ensembl.sub.txt
+    for speciesFile2 in $workdir+=*-ensembl.sub.txt
     do
 	species2=$(basename $speciesFile2)
 	species2=${species2//-ensembl.sub.txt/}
 	echo $species2
 	if [ $species1 != $species2 ]
 	   then
-	       (perl parseOMA.2.pl data/$species1\_$species2\_OMA.csv data/$species1-ensembl.sub.txt data/$species2-ensembl.sub.txt > data/$species1\_$species2\_OMA.withSyn.txt) 2>> failedOMAparses.txt
+	       (perl parseOMA.2.pl $workdir+=$species1\_$species2\_OMA.csv $workdir+=$species1-ensembl.sub.txt $workdir+=$species2-ensembl.sub.txt > $workdir+=$species1\_$species2\_OMA.withSyn.txt) 2>> failedOMAparses.txt
 	fi
     done
 done
 
 
 # Obsolete; uses fasta headers.
-#	(perl parseOMA.pl data/$species1\_$species2\_OMA.csv data/$species1.fa data/$species2.fa > data/$species1\_$species2\_OMA.withSyn.txt) 2>> failedOMAparses.txt
+#	(perl parseOMA.pl $workdir+=$species1\_$species2\_OMA.csv $workdir+=$species1.fa $workdir+=$species2.fa > $workdir+=$species1\_$species2\_OMA.withSyn.txt) 2>> failedOMAparses.txt
