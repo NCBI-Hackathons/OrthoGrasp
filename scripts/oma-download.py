8e7972391013dc18d314ba481384a05ab40c9a82
Phillip Rak <rak.phillip@gmail.com>
2018-07-12 15:17:08 -0500

    --Troubleshooting issue with args

--- scripts/oma-download.py
+++ scripts/oma-download.py
@@ -3,31 +3,34 @@ import requests
 import io
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("out-dir", help="sets the output directory for oma files",
-                    action="store_true")
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--out-dir", help="sets the output directory for oma files",
+                        action="store_true")
+    args = parser.parse_args()
 
-x = ['CAEEL',
-    'DANRE',
-    'DROME',
-    'HUMAN',
-    'MOUSE',
-    'PANTR',
-    'RATNO',
-    'TAKRU']
+    x = ['CAEEL',
+        'DANRE',
+        'DROME',
+        'HUMAN',
+        'MOUSE',
+        'PANTR',
+        'RATNO',
+        'TAKRU']
 
-# Submit a query to the server at https://omabrowser.org/cgi-bin/gateway.pl?f=PairwiseOrthologs&p1=[x]&p2=[y]&p3=EnsemblGene
-#  where x is the first species that we want to search against
-#  where y is the second species that we want to run the search against
-for i in x:
-    for j in x:
-        if i != j:
-            payload = {'f': 'PairwiseOrthologs',
-                      'p1': i,
-                      'p2': j,
-                      'p3': 'OMA'}
-            r = requests.request('GET', 'https://omabrowser.org/cgi-bin/gateway.pl', params=payload)
-            filename = str(args.out-dir) + str(i) + "_" + str(j) + "_" + "OMA.csv"
-            df = pd.read_csv(io.StringIO(r.content.decode('utf-8')), header=None, delimiter="\t")
-            df.to_csv(filename, header=None, index=None, sep="\t")
+    # Submit a query to the server at https://omabrowser.org/cgi-bin/gateway.pl?f=PairwiseOrthologs&p1=[x]&p2=[y]&p3=EnsemblGene
+    #  where x is the first species that we want to search against
+    #  where y is the second species that we want to run the search against
+    for i in x:
+        for j in x:
+            if i != j:
+                payload = {'f': 'PairwiseOrthologs',
+                          'p1': i,
+                          'p2': j,
+                          'p3': 'OMA'}
+                r = requests.request('GET', 'https://omabrowser.org/cgi-bin/gateway.pl', params=payload)
+                filename = str(args.out-dir) + str(i) + "_" + str(j) + "_" + "OMA.csv"
+                df = pd.read_csv(io.StringIO(r.content.decode('utf-8')), header=None, delimiter="\t")
+                df.to_csv(filename, header=None, index=None, sep="\t")
+
+main()
