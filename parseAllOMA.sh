5e491dd7e02627830cea4cc0588aaec11d4fd43c
Phillip Rak <rak.phillip@gmail.com>
2018-07-12 16:27:20 -0500

    --Imported perl scripts

---
+++ parseAllOMA.sh
@@ -0,0 +1,37 @@
+rm failedOMAparses.txt
+touch failedOMAparses.txt
+
+# This has all of the data for all of the OMA ids for all species.
+# cd data
+# wget https://omabrowser.org/All/oma-ensembl.txt.gz
+# gunzip oma-ensembl.txt.gz
+# cd ..
+
+# This effectively sets which species OMA ids can be converted from.
+for species in "CAEEL" "DANRE" "DROME" "HUMAN" "MOUSE" "PANTR" "RATNO" "TAKRU"
+do
+    grep ^$species data/oma-ensembl.txt > data/$species-ensembl.sub.txt
+done
+
+# For each of those species files defined above, find the species names and
+# run the parsing perl script for each pair of species.
+for speciesFile1 in data/*-ensembl.sub.txt
+do
+    species1=$(basename $speciesFile1)
+    species1=${species1//-ensembl.sub.txt/}
+    echo $species1
+    for speciesFile2 in data/*-ensembl.sub.txt
+    do
+	species2=$(basename $speciesFile2)
+	species2=${species2//-ensembl.sub.txt/}
+	echo $species2
+	if [ $species1 != $species2 ]
+	   then
+	       (perl parseOMA.2.pl data/$species1\_$species2\_OMA.csv data/$species1-ensembl.sub.txt data/$species2-ensembl.sub.txt > data/$species1\_$species2\_OMA.withSyn.txt) 2>> failedOMAparses.txt
+	fi
+    done
+done
+
+
+# Obsolete; uses fasta headers.
+#	(perl parseOMA.pl data/$species1\_$species2\_OMA.csv data/$species1.fa data/$species2.fa > data/$species1\_$species2\_OMA.withSyn.txt) 2>> failedOMAparses.txt
---
+++ parseOMA.2.pl
@@ -0,0 +1,53 @@
+#!/opt/local/bin/perl -w
+
+use strict;
+no warnings 'uninitialized';
+
+if($#ARGV < 0) {
+    print STDERR "Useage: $0 < OMAid Species Pairwise > < Species Ref >\n";
+  exit;
+}
+
+#
+# This script takes a set of Pairwise orthology relationships for a pair of
+# species keyed by OMA ID (downloaded from https://omabrowser.org/oma/genomePW/)
+# as well as an OMA reference file (https://omabrowser.org/All/oma-ensembl.txt.gz),
+# and returns the pairwise relationship with synonyms substituted for
+# OMA ids (the synonyms are found in the reference file).
+#
+# Example:
+# perl parseOMA.pl data/HUMAN_DROME_OMA.csv data/HUMAN-ensembl.sub.txt data/DROME-ensembl.sub.txt > data/HUMAN_DROME_OMA.withSyn.csv
+
+my $omaFile = $ARGV[0];
+my $omaRef1 = $ARGV[1];
+my $omaRef2 = $ARGV[2];
+my (%synonyms);
+
+my $seq = "";
+my $header = "";
+
+for my $ref ($omaRef1,$omaRef2){
+    open (REF,$ref);
+    while (<REF>){
+	chomp $_;
+	my ($omaID,$refID) = split (/\t/,$_);
+	$refID =~ s/transcript_id=//g;
+	$refID =~ s/ /,/g;
+	if (exists($synonyms{$omaID})){
+	    $synonyms{$omaID} .= ",$refID";
+	}else {
+	    $synonyms{$omaID} = "$refID";
+	}
+    }
+    close REF;
+}
+
+open (OMA, $omaFile);
+while (<OMA>){
+    chomp $_;
+    my($omaID1,$omaID2,$relationship,$number) = split(/\t/,$_);
+#    print STDERR "$omaID1\t$omaID2\n";
+    my $synList1 = $synonyms{$omaID1};
+    my $synList2 = $synonyms{$omaID2};
+    print "$omaID1,$synList1\t$omaID2,$synList2\t$relationship\t$number\n";
+}
