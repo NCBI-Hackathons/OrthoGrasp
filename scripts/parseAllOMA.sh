a01c8d0ec98b9c0157028dd6ee9bd63a3a1ab647
Phillip Rak <rak.phillip@gmail.com>
2018-07-12 17:45:28 -0500

    --Properly referenced variables
      Updated destination for loops

--- scripts/parseAllOMA.sh
+++ scripts/parseAllOMA.sh
@@ -9,7 +9,7 @@
 workdir=/OrthoGrasp/data/
 scriptdir=/OrthGrasp/scripts/
 
-touch workdir+=failedOMAparses.txt
+touch $workdir+=failedOMAparses.txt
 
 # This effectively sets which species OMA ids can be converted from.
 for species in "CAEEL" "DANRE" "DROME" "HUMAN" "MOUSE" "PANTR" "RATNO" "TAKRU"
@@ -19,19 +19,19 @@ done
 
 # For each of those species files defined above, find the species names and
 # run the parsing perl script for each pair of species.
-for speciesFile1 in $workdir+=*-ensembl.sub.txt
+for speciesFile1 in ../data/*-ensembl.sub.txt
 do
     species1=$(basename $speciesFile1)
     species1=${species1//-ensembl.sub.txt/}
     echo $species1
-    for speciesFile2 in $workdir+=*-ensembl.sub.txt
+    for speciesFile2 in ../data/*-ensembl.sub.txt
     do
 	species2=$(basename $speciesFile2)
 	species2=${species2//-ensembl.sub.txt/}
 	echo $species2
 	if [ $species1 != $species2 ]
 	   then
-	       (perl scriptsdir+=parseOMA.2.pl $workdir+=$species1\_$species2\_OMA.csv $workdir+=$species1-ensembl.sub.txt $workdir+=$species2-ensembl.sub.txt > $workdir+=$species1\_$species2\_OMA.withSyn.txt) 2>> failedOMAparses.txt
+	       (perl $scriptsdir+=parseOMA.2.pl $workdir+=$species1\_$species2\_OMA.csv $workdir+=$species1-ensembl.sub.txt $workdir+=$species2-ensembl.sub.txt > $workdir+=$species1\_$species2\_OMA.withSyn.txt) 2>> failedOMAparses.txt
 	fi
     done
 done
